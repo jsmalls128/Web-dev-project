341e560d0602caa5f307aa407dda218ec96972d6
Jamal <jsmalls128@live.com>
2018-12-04 16:44:37 -0500

    login changes

--- frisbee/views.py
+++ frisbee/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 # Create your views here.
 from django.http import HttpResponse
-
+from frisbee.forms import LoginForm
 
 def index(request):
     return render(request,'home.html')
+
+def login(request):
+  username = "not logged in"
+  if request.method == "POST":
+    #Get the posted form
+    MyLoginForm = LoginForm(request.POST)
+    if MyLoginForm.is_valid():
+      username = MyLoginForm.cleaned_data['username']
+    return redirect(loginerror)
+  else:
+    MyLoginForm = LoginForm()
+  return render(request,'login.html')
